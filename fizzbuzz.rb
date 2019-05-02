302dbc70f793608e24ec2fa6abcb3ce3be2002b4
Dennis <dennis.basyrov@gmail.com>
2019-05-02 05:24:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
-require_relative './spec_helper.rb'
-require_relative '../fizzbuzz.rb'
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
 
 def fizzbuzz(num)
   if num = 3
